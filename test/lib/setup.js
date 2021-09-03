bdd23b446e4a9708b55f5764fcd14c31d4ed4c8a
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-09-03 22:45:55 +0200

    inherit no longer necessary to debug

--- test/lib/setup.js
+++ test/lib/setup.js
@@ -320,11 +320,11 @@ function installJsController(cb) {
             setTimeout(function () {
                 client.destroy();
                 if (!fs.existsSync(rootDir + 'tmp/node_modules/' + appName + '.js-controller')) {
-                    console.log('installJsController: no js-controller => install from git');
+                    console.log('installJsController: no js-controller => install dev build from npm');
 
                     child_process.execSync('npm install ' + appName + '.js-controller@dev --prefix ./ --production', {
                         cwd:   rootDir + 'tmp/',
-                        stdio: ['inherit']
+                        stdio: [0, 1, 2]
                     });
                 } else {
                     console.log('Setup js-controller...');
